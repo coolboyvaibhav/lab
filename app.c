26152daee8650b363758399dd1bc0a3c0c951a08
Vaib <vaibhav.shatalwar07@gmail.com>
2023-12-16 23:15:45 +0530

    first trial of push

---
+++ app.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hello worldLLLLL");
+
+    return 0;
+}
+
+//git config --global user.email "vaibhav.shatalwar07@gmail.com"
+//   git config --global user.name "Vaib"
